1c0f18157ed8a85ce01098edccd1bb4a4bde52b6
Carolin Chhor <carolin-vg@Carolins-Air.webpass.net>
2026-01-15 19:27:27 -0800

    refactor: disable automatic cell merging in excel export to match import style

--- src/lib/excel-export.ts
+++ src/lib/excel-export.ts
@@ -32,23 +32,6 @@ export const exportToExcel = (data: any[], headers: string[], columnMetadata: Co
     // 2. Create worksheet
     const worksheet = utils.aoa_to_sheet(worksheetData);
 
-    // 3. Handle Merged Cells for Groups if applicable
-    if (hasGroups) {
-        const merges: any[] = [];
-        let startCol = 0;
-        const groupRow = worksheetData[0];
-
-        for (let i = 1; i <= groupRow.length; i++) {
-            if (i === groupRow.length || groupRow[i] !== groupRow[startCol]) {
-                if (i - 1 > startCol && groupRow[startCol] !== '') {
-                    merges.push({ s: { r: 0, c: startCol }, e: { r: 0, c: i - 1 } });
-                }
-                startCol = i;
-            }
-        }
-        worksheet['!merges'] = merges;
-    }
-
     // 4. Create workbook and save
     const workbook = utils.book_new();
     utils.book_append_sheet(workbook, worksheet, 'Data');
